d9fefae70658512426e8dd5605fc7e0477378178
TjanL <tljubesek@gmail.com>
2023-11-15 23:16:29 +0100

    add access granted message

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,21 @@ import LineScramble from "./LineScramble";
 
 function App() {
     const [loggedin, setLoggedin] = useState(false);
+    const [hideMessage, setHideMessage] = useState(false);
     const [lines] = useState([
         { text: "Crack the enigma embedded in the code and unlock the complete details of your mission.", scramble: false },
         { text: "If decoding proves challenging, seek Agent Mojca for discreet assistance in your private chambers to uncover additional clues.", scramble: false },
-        { text: "Lat: 123.000, Lng: 123.00", scramble: true },
+        { text: "Lat: 123.00", scramble: true },
+        { text: "Lng: 123.00", scramble: true },
     ]);
     const [index, setIndex] = useState(1);
 
     const handleLogin = (password) => {
         if (password.toLowerCase() == "racoon") {
             setLoggedin(true);
+            setTimeout(() => {
+                setHideMessage(true);
+            }, 1500);
         } else {
             window.location.href = "https://www.cia.gov/";
         }
@@ -32,20 +37,26 @@ function App() {
                         <Login submit={handleLogin} />
                     </div>
                 ) : (
-                    <div className="flex flex-col p-1 text-xl font-semibold font-['Courier_New'] gap-1">
-                        {
-                            lines.slice(0, index).map((line) => (
-                                (!line.scramble) ? (
-                                    <Line text={line.text} onComplete={onComplete} />
-                                ) : (
-                                    <LineScramble text={line.text} onComplete={onComplete} />
-                                )
-                            ))
-                        }
-                    </div>
+                    (!hideMessage) ? (
+                        <div className="p-2 h-screen flex flex-col justify-center">
+                            <div className="text-center text-5xl font-bold font-['Courier_New']">Access granted</div>
+                        </div>
+                    ) : (
+                        <div className="flex flex-col p-1 text-xl font-semibold font-['Courier_New'] gap-1">
+                            {
+                                lines.slice(0, index).map((line) => (
+                                    (!line.scramble) ? (
+                                        <Line text={line.text} onComplete={onComplete} />
+                                    ) : (
+                                        <LineScramble text={line.text} onComplete={onComplete} />
+                                    )
+                                ))
+                            }
+                        </div>
+                    )
                 )
             }
-        </div>
+        </div >
     )
 }
 
